f8dcc19bd9e31e2e25f4dc4644775c2dc1ce4090
a013898 <julien.arsonneau@renault.com>
2020-12-09 15:27:20 +0100

    end the nav

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const navSlade = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
-
+    const navLinks = document.querySelectorAll('.nav-links li');
+    //toggle nav
     burger.addEventListener('click', () =>{
+        //toggle nav
         nav.classList.toggle('nav-active');
+
+        //animation des liens
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = '';
+            }else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`;
+            }
+        });
+        //animation burger
+        burger.classList.toggle('toggle');
     });
 }
 navSlade();
